5b187c3846be80e0f61f6e75515bbbf269d6ad90
Horizon <horizon3833@gmail.com>
2022-02-12 09:38:32 +0530

    bug fixed

--- script/install.ps1
+++ script/install.ps1
@@ -1,20 +1,20 @@
 $BinDir = "$Home/Desktop/HorizonExtensions"
 $ZipUrl = "https://github.com/Horizon3833/HorizonExtensions/releases/download/1.0/Extensions.zip"
-function UnzipFiles{
-    param(
-        [string]$zipFile, [string]$outPath
-    )
-    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipFile, $outPath)
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
 }
 
 function CreateDirectory {
     param (
-        $DirectoryName
-    )
-    if(-not(Test-Path -Path $DirectoryName)){
+          $DirectoryName
+      )
+      if (-not (Test-Path -Path $DirectoryName)) {
         New-Item -ItemType directory -Path $DirectoryName
-    }
-}
+      }
+  }
 
 CreateDirectory $BinDir
 
@@ -23,7 +23,7 @@ Invoke-WebRequest $ZipUrl -OutFile $BinDir
 Write-Output -ForegroundColor Green Done!
 
 Write-Output Extracting Files...
-UnzipFiles "$BinDir/Extensions.zip" "$BinDir/Extensions"
+Unzip "$BinDir/Extensions.zip" "$BinDir/Extensions"
 Remove-Item "$BinDir/Extensions.zip"
 Write-Output -ForegroundColor Green Extraction Done!!!
 Write-Output The Extensions are available at $BinDir/Desktop/HorizonExtensions/Extensions
